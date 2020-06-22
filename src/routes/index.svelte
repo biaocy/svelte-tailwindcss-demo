a05bad0f2f40c3e9e0b1764caf040c9a6f8ba7bb
Bill Chan <biaocy91@gmail.com>
2020-06-22 17:53:36 +0800

    Demon

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+	import {notifier, NotificationDisplay} from '@beyonk/svelte-notifications';
+
+	let timeout = 3000;
+	const test = () => {
+		notifier.success("I'll try my best");
+	}
+</script>
+<style>
+	.btn {
+		@apply p-2;
+		@apply border;
+    @apply bg-blue-400;
+    @apply text-white;
+	}
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<NotificationDisplay {timeout} />
+
+<button class="btn" on:click={test}>Test</button>
