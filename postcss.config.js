a35952b513bf987df41697cd80a81fa4a32c6fdb
Bill Chan <biaocy91@gmail.com>
2020-06-22 17:54:44 +0800

    Fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 // only needed if you want to purge
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.svelte", "./src/**/*.html"],
+  whitelist: ["toasts", "toast", "content", "progress"],
   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 });
 
